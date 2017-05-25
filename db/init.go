15641479896e618c84ca30127f6c89a94a95ecd1
medyagh <medya.ghazizadeh@target.com>
2017-05-25 16:37:33 -0500

    making create keyspace optional, move duplicate code to sharedlib

--- db/init.go
+++ db/init.go
@@ -4,67 +4,33 @@ import (
 	"github.com/k8guard/k8guard-action/db/stmts"
 
 	"fmt"
-	"time"
 
 	"github.com/gocql/gocql"
 	libs "github.com/k8guard/k8guardlibs"
+	libsdb "github.com/k8guard/k8guardlibs/db"
 )
 
 // Here we'll store connection
 var Sess *gocql.Session
+var err error
 
-// This is wrapper for gocql
-func Connect(hosts []string) error {
+func InitDB() {
+	Sess = libsdb.Connect(libs.Cfg.CassandraHosts)
 
-	libs.Log.Info("Connecting to db")
-
-	// Creating cluster for cassandra
-	cluster := gocql.NewCluster(hosts...)
-	cluster.Consistency = gocql.LocalQuorum
-	cluster.Timeout = time.Second * 15
-	// Auth if username is set
-	if libs.Cfg.CassandraUsername != "" {
-		libs.Log.Debug("Connecting with username", libs.Cfg.CassandraUsername)
-		cluster.Authenticator = gocql.PasswordAuthenticator{
-			Username: libs.Cfg.CassandraUsername,
-			Password: libs.Cfg.CassandraPassword,
-		}
-	}
-	if libs.Cfg.CassandraCaPath != "" {
-		libs.Log.Debug("Using Ca")
-		cluster.SslOpts = &gocql.SslOptions{
-			CaPath:                 libs.Cfg.CassandraCaPath,
-			EnableHostVerification: libs.Cfg.CassandraSslHostValidation,
-		}
-	}
-
-	// Initializing session
-	session, err := cluster.CreateSession()
-	if err != nil {
-		return err
-	}
-
-	// Storring cassandra session
-	Sess = session
-
-	// Initializing database
-	err = initDB()
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
-// This we need to initialize database scheme
-func initDB() error {
+func createDB() error {
 
-	libs.Log.Info("Initing DB")
+	libs.Log.Info("Initializing the DB schema ")
 
 	// Creating KEYSPACE (if not exists)
-	err := Sess.Query(fmt.Sprintf(stmts.CREATE_KEYSPACE, libs.Cfg.CassandraKeyspace)).Exec()
-	if err != nil {
-		return err
+	// some cassandras config may not allow Create keyspace
+	if libs.Cfg.CassandraCreateKeyspace {
+
+		err = Sess.Query(fmt.Sprintf(stmts.CREATE_KEYSPACE, libs.Cfg.CassandraKeyspace)).Exec()
+		if err != nil {
+			return err
+		}
 	}
 
 	err = Sess.Query(fmt.Sprintf(stmts.CREATE_VACTION_TABLE, libs.Cfg.CassandraKeyspace)).Exec()
--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"github.com/k8guard/k8guard-action/db"
 	"github.com/k8guard/k8guard-action/messaging"
 
 	libs "github.com/k8guard/k8guardlibs"
+
+	"github.com/revel/modules/db/app"
 )
 
 func main() {
 	libs.Log.Info("Hello From k8guard-action")
-
-	err := db.Connect(libs.Cfg.CassandraHosts)
-	if err != nil {
-		panic(err.Error())
-	}
+	db.Init()
 	messaging.ConsumeMessages()
 
 }
